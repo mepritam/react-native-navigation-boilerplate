703ffb99342617c7fdd582c9d2208f3c5379c825
pritamsadhu <pritam@pritamsadhu.in>
2019-03-21 21:04:26 +0530

    react native navigation ios configuration

--- index.js
+++ index.js
@@ -2,8 +2,19 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
+import { Navigation } from "react-native-navigation";
 import App from './App';
-import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+// register component
+Navigation.registerComponent(`App`, () => App);
+
+// set up component naviagtion 
+Navigation.events().registerAppLaunchedListener(() => {
+  Navigation.setRoot({
+    root: {
+      component: {
+        name: "App"
+      }
+    }
+  });
+});
